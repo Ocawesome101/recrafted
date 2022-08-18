283de8338adb7640808b4315e87418448962be6a
Ocawesome101 <ocawesome101@gmail.com>
2022-08-18 12:08:09 -0400

    theoretically almost finish installer

--- installer.lua
+++ installer.lua
@@ -1,7 +1,7 @@
 -- Recrafted installer
 
 local function dl(f)
-  local hand, err = http.get(url..f, nil, true)
+  local hand, err = http.get(f, nil, true)
   if not hand then
     error(err, 0)
   end
@@ -20,22 +20,24 @@ package.loaded.rc = {
 
 package.loaded.term = term
 package.loaded.colors = colors
+_G.require = require
 
 function term.at(x, y)
   term.setCursorPos(x, y)
   return term
 end
 
-local function ghload(f)
+local function ghload(f, c)
   return assert(load(dl("https://raw.githubusercontent.com/"..f),
-    "="..f, "t", _G))()
+    "="..(c or f), "t", _G))()
 end
 
-local json = ghload("rxi/json.lua/master/json.lua")
+local json = ghload("rxi/json.lua/master/json.lua", "ghload(json)")
 package.loaded.json = json
 
 local function rcload(f)
-  return ghload("ocawesome101/recrafted/primary/data/computercraft/lua/rom/"..f)
+  return ghload(
+    "ocawesome101/recrafted/primary/data/computercraft/lua/rom/"..f, f)
 end
 
 -- get recrafted's textutils with its extra utilities
@@ -52,12 +54,65 @@ local function progress(y, a, b)
 end
 
 term.at(1,1).clear()
-tu.coloredPrint(colors.yellow, "Recrafted Installer 1.0")
+tu.coloredPrint(colors.yellow,
+  "Recrafted Installer 1.0\n=======================")
 
+tu.coloredPrint(colors.white, "Installing Recrafted to ", colors.lightBlue,
+  "/rc", colors.white)
 
+local function bullet(t)
+  tu.coloredWrite(colors.red, "- ", colors.white, t)
+end
+
+local function ok()
+  tu.coloredPrint(colors.green, "OK", colors.white)
+end
+
+bullet("Getting repository tree...")
 
 local repodata = dl("https://api.github.com/repos/ocawesome101/recrafted/git/trees/primary?recursive=1")
 
 repodata = json.decode(repodata)
 
+ok()
 
+bullet("Filtering files...")
+local look = "data/computercraft/lua/"
+local to_dl = {}
+for _, v in pairs(repodata.tree) do
+  if v.path and v.path:sub(1,#look) == look then
+    v.path = v.path:sub(#look+1)
+    v.real_path = v.path:gsub("^/?rom", "rc")
+    to_dl[#to_dl+1] = v
+  end
+end
+ok()
+
+bullet("Creating directories...")
+for i=#to_dl, 1, -1 do
+  local v = to_dl[i]
+  if v.type == "tree" then
+    fs.makeDir(fs.combine(v.real_path))
+    table.remove(to_dl, i)
+  end
+end
+ok()
+
+bullet("Downloading files...")
+local okx, oky = term.getCursorPos()
+io.write("\n")
+local _, pby = term.getCursorPos()
+for i=1, #to_dl, 1 do
+  local v = to_dl[i]
+  if v.type == "blob" then
+    progress(pby, i, #to_dl)
+    local data = dl("https://raw.githubusercontent.com/ocawesome101/recrafted/primary/data/computercraft/lua/"..v.path)
+    assert(io.open(v.real_path, "w")):write(data):close()
+  end
+end
+term.clearLine()
+term.at(okx, oky)
+ok()
+
+settings.set("")
+settings.save()
